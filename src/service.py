3dcf2acd8e1899b836518278fb7e74e502275c30
Nirrax <Nirraxx@gmail.com>
2024-01-12 16:10:54 +0100

    Implementing generateMfccFromFile function

--- src/service.py
+++ src/service.py
@@ -19,3 +19,38 @@ def convert_mp3_to_wav(filepath, filename):
   
   audio = AudioSegment.from_mp3(filepath+'.mp3')
   audio.export(filename+'.wav', format='wav')    
+  
+def prepare_file(file_path, n_mfcc=13, n_fft=2048, hop_length=512):
+
+  SAMPLE_RATE = 22050
+  mfccs = []
+
+  # load file with librosa
+  signal, sr = librosa.load(file_path, sr=SAMPLE_RATE)
+  duration = math.floor(librosa.get_duration(y=signal, sr=SAMPLE_RATE))
+
+  number_of_segments = int(duration / 3)
+  samples_per_track = SAMPLE_RATE * duration
+  number_of_samples_per_segment = int(samples_per_track / number_of_segments)
+  expected_number_of_mfcc_vectors_per_segment = 130
+    
+  for segment in range(number_of_segments):
+      start_sample = number_of_samples_per_segment * segment
+      finish_sample = start_sample + number_of_samples_per_segment
+    
+      # generate mfcc for a segment
+      mfcc = librosa.feature.mfcc(y=signal[start_sample:finish_sample],
+                                  sr=sr,
+                                  n_fft=n_fft,
+                                  n_mfcc=n_mfcc,
+                                  hop_length=hop_length) 
+      mfcc = mfcc.T
+      
+      #ensure that mfcc count does not exceed expected value
+      if len(mfcc) > expected_number_of_mfcc_vectors_per_segment:
+        mfcc = np.delete(mfcc, 1, axis=0)
+        
+      if len(mfcc) == expected_number_of_mfcc_vectors_per_segment:
+        mfccs.append(mfcc.tolist())
+
+  return np.array(mfccs)
