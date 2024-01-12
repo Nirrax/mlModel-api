cc42773766f8f08dd24ac97bd2faa663e386914f
Nirrax <Nirraxx@gmail.com>
2024-01-12 16:06:01 +0100

    Implementing saveFileFromRequest funciton

--- src/service.py
+++ src/service.py
@@ -1,1 +1,16 @@
 from dependencies import *
+
+async def save_file_from_request(request, filepath):
+  try:
+    
+    #parse data from request
+    data = await request.json()
+    base64_data = data.get('file')
+    binary_data = base64.b64decode(base64_data)
+    
+    #save file to mp3
+    with open (filepath+'.mp3', 'wb') as file:
+      file.write(binary_data)  
+      
+  except Exception as e:
+    print(f'Error: {str(e)}')
