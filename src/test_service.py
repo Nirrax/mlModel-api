e7dd303fdfc183899e1e3d9c8a9f88ed16cab77e
Nirrax <Nirraxx@gmail.com>
2024-11-01 21:35:24 +0100

    feat: Add test for is_filename_unique function

--- src/test_service.py
+++ src/test_service.py
@@ -4,14 +4,20 @@ from service import *
 @pytest.mark.parametrize(
     "input_dict, expected",
     [
-        ({"a": 1, "b": 5, "c": 3, "d": 2}, "b"),          # Regular case
-        ({"a": 10, "b": 5, "c": 30, "d": 20}, "c"),       # Another regular case
-        ({"a": 1, "b": 1, "c": 1, "d": 1}, "a"),          # Dictionary with equal values
-        ({}, None),                                       # Empty dictionary
-        ({"a": 1}, "a"),                                  # Single element
-        ({"a": -1, "b": -5, "c": -3}, "a")                # Negative values
+        ({"a": 1, "b": 5, "c": 3, "d": 2}, "b"),
+        ({"a": 10, "b": 5, "c": 30, "d": 20}, "c"),
+        ({"a": 1, "b": 1, "c": 1, "d": 1}, "a"),
+        ({}, None),                                  
+        ({"a": 1}, "a"),                
+        ({"a": -1, "b": -5, "c": -3}, "a")
     ]
 )
 def test_get_key_with_max_value(input_dict, expected):
     assert get_key_with_max_value(input_dict) == expected
-    
+
+def test_is_filename_unique():
+    assert is_filename_unique("file1", []) is True
+    assert is_filename_unique("file1", ["file1"]) is False
+    assert is_filename_unique("file1", ["file1","file2"]) is False
+    assert is_filename_unique("file1", ["file2"]) is True
+    assert is_filename_unique("", []) is False
